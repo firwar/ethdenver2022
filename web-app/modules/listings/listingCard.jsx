d5c0bab5d1519abed0d73f6c8635021457654e3e
Jason Liu <jason.liu@miralabs.io>
2022-02-19 00:46:40 -0800

    Added flags around each contract call to know which ones fail and to retry them a few more times

--- web-app/modules/listings/listingCard.jsx
+++ web-app/modules/listings/listingCard.jsx
@@ -38,24 +38,94 @@ export const ListingCard = ({ listingAddress }) => {
     }
     // TODO: Get information from contract
     async function setData() {
-      setEscrow(
-        ethers.utils.formatEther(
-          await contract.connect(signer).listingMinEscrow()
-        )
-      );
-      setDescription(await contract.connect(signer).listingDescription());
-      setTitle(await contract.connect(signer).listingTitle());
-      setLocation(await contract.connect(signer).listingLocation());
-      const ipfsHash = await contract.connect(signer).listingImageLink();
-      if (ipfsHash.length < 46) {
-        setImageLink("//v2.grommet.io/assets/IMG_4245.jpg");
-      } else {
-        setImageLink(`https://gateway.pinata.cloud/ipfs/${ipfsHash}`);
-      }
-      const bigNumberPrice = await contract.connect(signer).listingPrice();
-      const bigNumberEscrow = await contract.connect(signer).listingMinEscrow();
-      setPrice(ethers.utils.formatEther(bigNumberPrice));
-      setEscrow(ethers.utils.formatEther(bigNumberEscrow));
+
+      //define a falg for each contract call
+      var flag1 = new Boolean(false);
+      var flag2 = new Boolean(false);
+      var flag3 = new Boolean(false);
+      var flag4 = new Boolean(false);
+      var flag5 = new Boolean(false);
+      var flag6 = new Boolean(false);
+      var flag7 = new Boolean(false);
+
+      //define a max number of times to retry the request and associated counter
+      const MAX_TRIES = 10;
+      var iterations = 0;
+
+      //Try for 7 api hits
+      do {
+        //if iterations is larger than max tries break from do loop, else increment and continue
+        if(iterations++ > MAX_TRIES) 
+        {
+          break;
+        }
+
+        //if the await function call hits the 40 requests per second limit it will throw an
+        //error and enter the catch statement and not set the associated flag to true.
+        //On the next iteration the contract will be called again.
+        try{
+          if(flag1 == false)
+          {
+            setEscrow(
+              ethers.utils.formatEther(
+                await contract.connect(signer).listingMinEscrow()
+              )
+            );
+            flag1 = true;
+          }
+          if(flag2 == false)
+          {
+            setDescription(await contract.connect(signer).listingDescription());
+            flag2 = true;
+          }
+          if(flag3 == false)
+          {
+            setTitle(await contract.connect(signer).listingTitle());
+            flag3 = true;
+          }
+          if(flag4 == false)
+          {
+            setLocation(await contract.connect(signer).listingLocation());
+            flag4 = true;
+          }
+          if(flag5 == false)
+          {
+            const ipfsHash = await contract.connect(signer).listingImageLink();
+
+            if (ipfsHash.length < 46) {
+              setImageLink("//v2.grommet.io/assets/IMG_4245.jpg");
+            } else {
+              setImageLink(`https://gateway.pinata.cloud/ipfs/${ipfsHash}`);
+            }
+            flag5= true;
+          }
+          if(flag6 == false)
+          {
+            const bigNumberPrice = await contract.connect(signer).listingPrice();
+            setPrice(ethers.utils.formatEther(bigNumberPrice));
+            flag6 = true;
+          } 
+          if(flag7 == false)
+          {
+            const bigNumberEscrow = await contract.connect(signer).listingMinEscrow();
+            setEscrow(ethers.utils.formatEther(bigNumberEscrow));
+            flag7 = true;
+          }
+        }catch(error)
+        {
+          console.log(error);
+          //Adding a delay before retrying to slow down requests per second
+          //Increasing backoff window
+          await new Promise(r => setTimeout(r, 50 + iterations * 50));
+        }
+
+      }while(flag1 == false || 
+              flag2 == false || 
+              flag3 == false || 
+              flag4 == false || 
+              flag5 == false || 
+              flag6 == false || 
+              flag7 == false );
     }
     setData();
   }, [contract, signer]);
